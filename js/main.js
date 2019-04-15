3e0441a43b11eff42c725688c9f90536dd4ecb58
michelle9193 <michellefortuin93@gmail.com>
2019-04-15 19:59:45 +0200

    added image of myself

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Back to top button
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 400) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
