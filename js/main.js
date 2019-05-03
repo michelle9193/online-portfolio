366e210a844161e0b9695c1a2bae987930b69198
michelle9193 <michellefortuin93@gmail.com>
2019-05-03 14:07:58 +0200

    added new project to portfolio

--- js/main.js
+++ js/main.js
@@ -15,3 +15,20 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+$(document).ready(function () {
+
+  $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
+  });
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+  $('.third-button').on('click', function () {
+
+    $('.animated-icon3').toggleClass('open');
+  });
+});
